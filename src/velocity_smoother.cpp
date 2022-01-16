6a3677a7bec575690ef8474e7c3eb0ed3bb1549d
Chris Lalancette <clalancette@openrobotics.org>
2022-01-16 13:19:30 +0000

    Small fix to the velocity smoother.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- src/velocity_smoother.cpp
+++ src/velocity_smoother.cpp
@@ -187,7 +187,7 @@ void VelocitySmoother::timerCB()
     input_active_ = false;
     if (target_vel_.linear.x != 0.0 || target_vel_.angular.z != 0.0)
     {
-      RCLCPP_WARN(get_logger(), "Velocity Smoother : input went inactive leaving us a non-zero target velocity (%d, %d), zeroing...",
+      RCLCPP_WARN(get_logger(), "Velocity Smoother : input went inactive leaving us a non-zero target velocity (%f, %f), zeroing...",
                   target_vel_.linear.x,
                   target_vel_.angular.z);
       target_vel_ = geometry_msgs::msg::Twist();
