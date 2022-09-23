a671cece178f9bf8ef428e538548d4aa69d181c6
Chris Lalancette <clalancette@openrobotics.org>
2022-09-23 18:21:18 +0000

    Remove speed_lim_w internal variable.
    Get it from the node parameter database instead.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- include/velocity_smoother/velocity_smoother.hpp
+++ include/velocity_smoother/velocity_smoother.hpp
@@ -57,7 +57,7 @@ private:
   } feedback_;  /**< What source to use as feedback for smoothed velocity calculations */
 
   double accel_lim_v_, decel_lim_v_;
-  double speed_lim_w_, accel_lim_w_, decel_lim_w_;
+  double accel_lim_w_, decel_lim_w_;
 
   geometry_msgs::msg::Twist current_vel_;
   geometry_msgs::msg::Twist target_vel_;
