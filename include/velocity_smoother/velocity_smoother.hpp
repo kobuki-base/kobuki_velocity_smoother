ccd90543a7054e4cc8fa12e57be2180c1134b566
Chris Lalancette <clalancette@openrobotics.org>
2022-09-23 18:30:09 +0000

    Remove internal accel_lim_v.
    Instead, get the value directly from the node parameter database.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- include/velocity_smoother/velocity_smoother.hpp
+++ include/velocity_smoother/velocity_smoother.hpp
@@ -56,7 +56,6 @@ private:
     COMMANDS
   } feedback_;  /**< What source to use as feedback for smoothed velocity calculations */
 
-  double accel_lim_v_;
   double accel_lim_w_;
 
   geometry_msgs::msg::Twist current_vel_;
